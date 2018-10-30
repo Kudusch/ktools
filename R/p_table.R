23e564fe9453d1e630f0b6d2b46c7069d7f18f26
Kudusch <kudusch@posteo.de>
2018-10-30 12:35:42 +0100

    Add percentage argument to p_table

--- R/p_table.R
+++ R/p_table.R
@@ -1,9 +1,16 @@
 #' Faster prop.table(table())
 #'
 #' @param object An object to tabulate.
+#' @param percent Return rounded percentage.
 #' @param ... Other arguments passed to \link[base]{table}.
 #' @examples
 #' p_table(df)
-p_table <- function(object, ...) {
-    return(prop.table(table(object, ...)))
+p_table <- function(object, percent=TRUE, ...) {
+    if (percent == FALSE) {
+        return_obj <- prop.table(table(object, ...))
+    } else {
+        return_obj <- round(prop.table(table(object, ...)*100), 2)
+    }
+    names(dimnames(return_obj)) <- deparse(substitute(object))
+    return(return_obj)
 }
