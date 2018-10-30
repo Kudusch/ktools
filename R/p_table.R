aeea7988444e17433f5005480cd77535584b07f3
Kudusch <kudusch@posteo.de>
2018-10-30 16:06:48 +0100

    Fix bug in percentage argument in p_table

--- R/p_table.R
+++ R/p_table.R
@@ -9,7 +9,7 @@ p_table <- function(object, percent=TRUE, ...) {
     if (percent == FALSE) {
         return_obj <- prop.table(table(object, ...))
     } else {
-        return_obj <- round(prop.table(table(object, ...)*100), 2)
+        return_obj <- round(prop.table(table(object, ...))*100, 2)
     }
     names(dimnames(return_obj)) <- deparse(substitute(object))
     return(return_obj)
