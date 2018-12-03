d24d7ae3d77111c461f4028edb3fbfdee36ea384
Kudusch <kudusch@posteo.de>
2018-12-03 14:03:07 +0100

    Rename p_table to ptable

--- R/ptable.R
+++ R/ptable.R
@@ -4,14 +4,12 @@
 #' @param object An object to tabulate.
 #' @param percent Return rounded percentage.
 #' @param ... Other arguments passed to \link[base]{table}.
-#' @return \code{p_table()} returns a contingency table, an object of class "table", an array of integer values. See \link[base]{table}.
+#' @return \code{ptable()} returns a contingency table, an object of class "table", an array of integer values. See \link[base]{table}.
 #' @examples
-#'\dontrun{
-#' p_table(df, percent = FALSE, useNA = 'always')
-#'}
+#' ptable(c(1,1,1,2,2,3), percent = FALSE, useNA = 'always')
 #'
 #' @export
-p_table <- function(object, percent=TRUE, ...) {
+ptable <- function(object, percent=TRUE, ...) {
     if (percent == FALSE) {
         return_obj <- prop.table(table(object, ...))
     } else {
