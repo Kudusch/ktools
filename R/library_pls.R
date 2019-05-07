b86c1a3140e7817ee9425ece608e346a02e6a816
Tim Schatto-Eckrodt <kudusch@posteo.de>
2019-05-07 12:22:11 +0200

    Add library_pls() function

---
+++ R/library_pls.R
@@ -0,0 +1,36 @@
+#' Library please! Easier Package Installation
+#'
+#' @description If the last error was raised due to calling \code{library()} on a missing package.
+#' @param package The package to install.
+#' @examples
+#'\dontrun{
+#' library(parallel)
+#' # Error in library(parallel) : there is no package called ‘parallel’
+#'
+#' library_pls(parallel)
+#' # Install package parallel?
+#' # (y/n) y
+#' # Installing package …
+#'}
+#'
+#' @export
+library_pls <- function(package) {
+    if(interactive()) {
+        package_name <- deparse(substitute(package))
+        last_error <- .traceback()
+        if (!is.null(last_error)) {
+            if (last_error[[1]] == "stop(packageNotFoundError(package, lib.loc, sys.call()))") {
+                cat(paste("Install package ", package_name, "?\n", sep = ""))
+                if (readline(prompt="(y/n) ") == "y") {
+                    install.packages(package_name)
+                }
+            } else {
+                message("Nothing to do!")
+            }
+        } else {
+            message("Nothing to do!")
+        }
+    } else {
+        message(.traceback())
+    }
+}
